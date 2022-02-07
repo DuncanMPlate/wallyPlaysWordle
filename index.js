42cadcbc46067ef159de3326dbdb26f3a68bf691
Duncan Plate <duncanplate@gmail.com>
2022-02-07 04:29:33 -0600

    trying to get userauth to work

--- index.js
+++ index.js
@@ -36,6 +36,26 @@ app.get('/leader.html', function (req, res) {
 	res.sendFile(__dirname + "/" + "leader.html");
 });
 
+const { auth } = require('express-openid-connect');
+
+const config = {
+	authRequired: false,
+	auth0Logout: true,
+	secret: process.env.SECRET,
+	baseURL: process.env.BASE_URL,
+	clientID: process.env.CLIENT_ID,
+	issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+	res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
 app.get('/word', (req, res) => {
 	// console.log(res)
 	const file = fs.readFileSync(path.join(__dirname, '/words/words.txt'), 'utf8');
