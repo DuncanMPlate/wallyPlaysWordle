5041422513472b1b57db95944f32f705ced6b901
Duncan Plate <duncanplate@gmail.com>
2022-02-07 05:22:14 -0600

    reverting to restore a working version

--- index.js
+++ index.js
@@ -7,52 +7,52 @@ const cors = require("cors")
 require('dotenv').config() 
 const app = express()
 app.use(cors())
-const router = require('./routes/router')
-const auth = require('./middleware/auth');
-const requiresAuth = require('./middleware/requiresAuth');
-const attemptSilentLogin = require('./middleware/attemptSilentLogin');
+// const router = require('./routes/router')
+// const auth = require('./middleware/auth');
+// const requiresAuth = require('./middleware/requiresAuth');
+// const attemptSilentLogin = require('./middleware/attemptSilentLogin');
 
-module.exports = {
-	auth,
-	...requiresAuth,
-	attemptSilentLogin,
-};
-const { auth } = require('express-openid-connect');
+// module.exports = {
+// 	auth,
+// 	...requiresAuth,
+// 	attemptSilentLogin,
+// };
+// const { auth } = require('express-openid-connect');
 
-const config = {
-	authRequired: false,
-	auth0Logout: true,
-	secret: process.env.SECRET,
-	baseURL: process.env.BASE_URL,
-	clientID: process.env.CLIENT_ID,
-	issuerBaseURL: process.env.ISSUER_BASE_URL
-};
+// const config = {
+// 	authRequired: false,
+// 	auth0Logout: true,
+// 	secret: process.env.SECRET,
+// 	baseURL: process.env.BASE_URL,
+// 	clientID: process.env.CLIENT_ID,
+// 	issuerBaseURL: process.env.ISSUER_BASE_URL
+// };
 
-app.use('/', router);
+// app.use('/', router);
 
-// Catch 404 and forward to error handler
-app.use(function (req, res, next) {
-	const err = new Error('Not Found');
-	err.status = 404;
-	next(err);
-});
+// // Catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+// 	const err = new Error('Not Found');
+// 	err.status = 404;
+// 	next(err);
+// });
 
-// Error handlers
-app.use(function (err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: process.env.NODE_ENV !== 'production' ? err : {}
-	});
-});
+// // Error handlers
+// app.use(function (err, req, res, next) {
+// 	res.status(err.status || 500);
+// 	res.render('error', {
+// 		message: err.message,
+// 		error: process.env.NODE_ENV !== 'production' ? err : {}
+// 	});
+// });
 
 
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
+// // auth router attaches /login, /logout, and /callback routes to the baseURL
+// app.use(auth(config));
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, '/index.html',))
-	res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+	// res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
 
 const { requiresAuth } = require('express-openid-connect');
